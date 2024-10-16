93f05a271d7e67b596d0a0a643ec4c58dc1ac36e
scelo <scelogumede95@gmail.com>
2024-10-16 17:05:29 +0200

    shit

--- src/components/BlagCart.tsx
+++ src/components/BlagCart.tsx
@@ -4,20 +4,22 @@
 interface BlogCartProps {
     image:string,
     title:string,
-    text:string
+    text:string,
+    date?:string
 }
 
 
 const BlogCart = (props:BlogCartProps) => {
-    const{image,title,text}=props
+    const{image,title,text,date}=props
   return (
-    <article className=" overflow-hidden flex flex-col gap-4">
+    <article className={ date ?`flex  ` :`overflow-hidden flex flex-col gap-4`}>
         <div className="rounded-tl-md  rounded-tr-md overflow-hidden">
             <img src={image} className="h-full w-full" />
         </div>
         <div>
             <p className="font-semibold text-3xl ">{title}</p>
-            <p className="font-thin">{text}</p>
+            <p>{date && (<span>{String(date)}</span>)}</p>
+            {!date && (<p className="font-thin">{text}</p>)}
         </div>
     </article>
   )
